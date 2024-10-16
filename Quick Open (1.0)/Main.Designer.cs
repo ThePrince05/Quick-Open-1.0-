3d9a367bf243bbaa5c2ae8709ba462e6bf7231d2
Prince Sithole <princesithole49@gmail.com>
2024-10-16 06:36:12 +0200

    Restarted program becuase hotkey functionality didn't work. Have setup design of form and draggable.

---
+++ Quick Open (1.0)/Main.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Quick_Open__1._0_
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.btn_close = new FontAwesome.Sharp.IconPictureBox();
+            this.panel_border = new System.Windows.Forms.Panel();
+            this.lbl_startUp = new System.Windows.Forms.Label();
+            this.txt_directory = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.lbl_quickOpen = new System.Windows.Forms.Label();
+            this.lbl_main = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.panel_main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).BeginInit();
+            this.panel_border.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // panel_main
+            // 
+            this.panel_main.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(231)))), ((int)(((byte)(240)))));
+            this.panel_main.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_main.Controls.Add(this.btn_close);
+            this.panel_main.Controls.Add(this.panel_border);
+            this.panel_main.Controls.Add(this.txt_directory);
+            this.panel_main.Controls.Add(this.btn_save);
+            this.panel_main.Controls.Add(this.lbl_quickOpen);
+            this.panel_main.Controls.Add(this.lbl_main);
+            this.panel_main.Location = new System.Drawing.Point(3, 3);
+            this.panel_main.Margin = new System.Windows.Forms.Padding(0);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(635, 235);
+            this.panel_main.TabIndex = 1;
+            this.panel_main.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_main_MouseDown);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(231)))), ((int)(((byte)(240)))));
+            this.btn_close.ForeColor = System.Drawing.Color.Black;
+            this.btn_close.IconChar = FontAwesome.Sharp.IconChar.CircleXmark;
+            this.btn_close.IconColor = System.Drawing.Color.Black;
+            this.btn_close.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_close.IconSize = 49;
+            this.btn_close.Location = new System.Drawing.Point(581, 3);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.btn_close.Size = new System.Drawing.Size(49, 50);
+            this.btn_close.TabIndex = 6;
+            this.btn_close.TabStop = false;
+            // 
+            // panel_border
+            // 
+            this.panel_border.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_border.Controls.Add(this.lbl_startUp);
+            this.panel_border.Location = new System.Drawing.Point(542, 169);
+            this.panel_border.Name = "panel_border";
+            this.panel_border.Size = new System.Drawing.Size(83, 46);
+            this.panel_border.TabIndex = 5;
+            // 
+            // lbl_startUp
+            // 
+            this.lbl_startUp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_startUp.AutoSize = true;
+            this.lbl_startUp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_startUp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F);
+            this.lbl_startUp.Location = new System.Drawing.Point(4, 4);
+            this.lbl_startUp.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_startUp.Name = "lbl_startUp";
+            this.lbl_startUp.Size = new System.Drawing.Size(73, 36);
+            this.lbl_startUp.TabIndex = 4;
+            this.lbl_startUp.Text = "Start-up \r\nSettings\r\n";
+            // 
+            // txt_directory
+            // 
+            this.txt_directory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_directory.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F);
+            this.txt_directory.Location = new System.Drawing.Point(84, 90);
+            this.txt_directory.Name = "txt_directory";
+            this.txt_directory.Size = new System.Drawing.Size(469, 34);
+            this.txt_directory.TabIndex = 3;
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.btn_save.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F);
+            this.btn_save.Location = new System.Drawing.Point(233, 141);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(163, 41);
+            this.btn_save.TabIndex = 2;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = false;
+            // 
+            // lbl_quickOpen
+            // 
+            this.lbl_quickOpen.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_quickOpen.Location = new System.Drawing.Point(6, 5);
+            this.lbl_quickOpen.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_quickOpen.Name = "lbl_quickOpen";
+            this.lbl_quickOpen.Size = new System.Drawing.Size(97, 21);
+            this.lbl_quickOpen.TabIndex = 1;
+            this.lbl_quickOpen.Text = "Quick Open";
+            // 
+            // lbl_main
+            // 
+            this.lbl_main.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Underline);
+            this.lbl_main.Location = new System.Drawing.Point(75, 55);
+            this.lbl_main.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_main.Name = "lbl_main";
+            this.lbl_main.Size = new System.Drawing.Size(413, 32);
+            this.lbl_main.TabIndex = 0;
+            this.lbl_main.Text = "Select Application Directory";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 240);
+            this.Controls.Add(this.panel_main);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Main";
+            this.Text = "Quick Open";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.panel_main.ResumeLayout(false);
+            this.panel_main.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).EndInit();
+            this.panel_border.ResumeLayout(false);
+            this.panel_border.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Panel panel_main;
+        private FontAwesome.Sharp.IconPictureBox btn_close;
+        private System.Windows.Forms.Panel panel_border;
+        private System.Windows.Forms.Label lbl_startUp;
+        private System.Windows.Forms.TextBox txt_directory;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Label lbl_quickOpen;
+        private System.Windows.Forms.Label lbl_main;
+    }
+}
+
---
+++ Quick Open (1.0)/Main.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_Open__1._0_
+{
+    public partial class Main : Form
+    {
+        private string appDataFolder;
+        private string quickOpenFolder;
+        private string directoryFilePath;
+
+        // hotkey registration
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 1; // Identifier for the hotkey
+        private const uint MOD_CTRL = 0x0002;
+        private const uint MOD_ALT = 0x0001;
+        private const uint VK_F = 0x46; // Virtual Key for 'F'
+
+        // A flag to check if the hotkey was registered successfully
+        private bool hotkeyRegistered = false;
+
+
+        // Import user32.dll for draggable
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int iParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+
+        // tray menu
+        private ContextMenuStrip trayMenu;
+        public Main()
+        {
+            InitializeComponent();
+
+            // Register the Ctrl+Alt+F hotkey
+            hotkeyRegistered = RegisterHotKey(this.Handle, HOTKEY_ID, MOD_CTRL | MOD_ALT, VK_F);
+           
+            // Optionally, notify the user if registration failed
+            if (!hotkeyRegistered)
+            {
+              MessageBox.Show("Hotkey registration failed.", "Failure",
+              MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            // Set the form's icon
+            this.Icon = new System.Drawing.Icon("quick low.ico");
+
+            // Get AppData folder path and set paths for the folder and file
+            appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            quickOpenFolder = Path.Combine(appDataFolder, "Quick Open");
+            directoryFilePath = Path.Combine(quickOpenFolder, "directory.txt");
+
+            // Ensure the directory and file exist
+            EnsureDirectoryAndFileExist();
+
+
+            // Initialize context menu for NotifyIcon
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Open", null, OpenSetUp);
+            trayMenu.Items.Add("Exit", null, ExitApp);
+
+            //Attach the context menu to the NotifyIcon
+            notifyIcon1.ContextMenuStrip = trayMenu;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+
+            // Listen for the hotkey message
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                // Action when Ctrl+Alt+F is pressed
+                OpenApplication();
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void OpenApplication()
+        {
+            // Specify the path of the application you want to open
+            System.Diagnostics.Process.Start("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+
+            /* string path = ReadDirectoryPath();
+
+            try
+            {
+                // Specify the path to the program you want to open
+                Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong on: " + ex.Message);
+            }
+            */
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Unregister the hotkey when the form is closed
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+
+        // Method to restore the application from the tray
+        private void OpenSetUp(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+
+            notifyIcon1.Visible = false;
+
+            try
+            {
+                // Unregister the hotkey when the form is closed
+                if (hotkeyRegistered)
+                {
+                    UnregisterHotKey(this.Handle, HOTKEY_ID);
+                }
+
+                // Avoid recursion by preventing the event from triggering again
+                base.FormClosing -= Main_FormClosing;
+
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong on: " + ex.Message, "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        // This method ensures the folder and file exist, and creates them if necessary
+        private void EnsureDirectoryAndFileExist()
+        {
+            try
+            {
+                // Check if the "Quick Open" folder exists; if not, create it
+                if (!Directory.Exists(quickOpenFolder))
+                {
+                    Directory.CreateDirectory(quickOpenFolder);
+                    // MessageBox.Show("Quick Open folder created at: " + quickOpenFolder);  
+                }
+
+                // Check if the "directory.txt" file exists; if not, create it
+                if (!File.Exists(directoryFilePath))
+                {
+                    File.Create(directoryFilePath).Close(); // Create the file and close the stream
+                                                            // MessageBox.Show("Directory file created at: " + directoryFilePath);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong on: " + ex.Message, "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        // This method writes a new directory path to the "directory.txt" file
+        private void SaveDirectoryPath(string newPath)
+        {
+            try
+            {
+                // Overwrite the file with the new directory path
+                File.WriteAllText(directoryFilePath, newPath);
+                MessageBox.Show("Directory path saved to: " + directoryFilePath, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong on: " + ex.Message, "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string ReadDirectoryPath()
+        {
+            try
+            {
+                // Check if the file exists before reading it
+                if (File.Exists(directoryFilePath))
+                {
+
+                    // Read all text from the file
+                    string directoryPath = File.ReadAllText(directoryFilePath);
+
+                    // Check if the file is empty or contains only whitespace
+                    if (string.IsNullOrWhiteSpace(directoryPath))
+                    {
+                        MessageBox.Show("Please configure program settings.", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return null;
+                    }
+
+                    // Return the directory path if the file has content
+                    return directoryPath;
+
+                }
+                else
+                {
+                    MessageBox.Show("Oops, Something went wrong on: Directory File does not exist.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oops, something went wrong on: " + ex.Message, "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_main_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            // When the left mouse button is pressed, drag the window
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
